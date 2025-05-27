79a8fb4b09414eb94525d6fe122a65203c82486e
Ravishankarpanday8521 <ravishankar02062002@gmail.com>
2025-05-27 17:00:01 +0530

    gjfjg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ import ContactSection from "../components/ContactSection";
 import JoinTeamSection from "../components/JoinTeamSection";
 import Footer from "../components/Footer";
 
-// Import dataljwehfouhwe
+// Import dataljwehfouhwe   ghgh 
 import { blogContent, shlokas, team, videos } from "../data/content";
 
 export default function Home() {
